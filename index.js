284abc62a8eb85d0979ea7364c5b565e52936ece
djc74 <davidcooper974@gmail.com>
2017-10-20 21:44:50 +0100

    first test done

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //require dependencies
 const express = require('express');
 const morgan = require('morgan');
+const routes = require('./config/routes');
 const port = process.env.PORT || 3000;
+const expressLayouts = require('express-ejs-layouts');
+
+
 const app = express();
 
 //settings
@@ -9,8 +13,10 @@ app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
 //middleware
+app.use(expressLayouts);
 app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
+app.use(routes);
 //listen to PORT
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
